fe1472e1656e851ecdbb7c26dbdd63a9c0870eaf
Ana Clara Ferreira de Sá <ferreiradesaanaclara@gmail.com>
2025-01-28 12:57:08 -0300

    Melhoria no tratamento de exceções JogoTabuleiro.hpp

--- include/JogoTabuleiro.hpp
+++ include/JogoTabuleiro.hpp
@@ -5,18 +5,18 @@
 #include <iostream>
 #include <vector>
 #include <memory>
+#include <stdexcept> // Para exceções
 
 class JogoTabuleiro {
-protected:
+private:
     std::shared_ptr<Jogador> jogador1, jogador2;
     std::vector<std::vector<char>> tabuleiro;
     int dimensaoTabuleiro;
-    bool jogadorAtual;
+    bool jogadorAtual; // true para jogador1, false para jogador2
 
 public:
-    JogoTabuleiro(std::shared_ptr<Jogador> jogador1, std::shared_ptr<Jogador> jogador2, int n); // construtor mais completo
-
-    ~JogoTabuleiro(){};
+    JogoTabuleiro(std::shared_ptr<Jogador> jogador1, std::shared_ptr<Jogador> jogador2, int n); // Construtor
+    ~JogoTabuleiro() = default;
 
     // Leitura Jogada
     virtual void leJogada(char linha, int coluna);
@@ -24,11 +24,22 @@ public:
     // Validação Jogada
     virtual bool isJogadaValida(char linha, int coluna);
 
-    // Printar Jogador
+    // Printar Tabuleiro
     virtual void printaTabuleiro() const;
 
     // Testa Condição de Vitória
-    virtual bool isVitoria() = 0;
+    virtual bool isVitoria();
+
+    // Exceções Específicas
+    class JogadaInvalidaException : public std::runtime_error {
+    public:
+        explicit JogadaInvalidaException(const std::string& mensagem) : std::runtime_error(mensagem) {}
+    };
+
+    class DimensaoInvalidaException : public std::invalid_argument {
+    public:
+        explicit DimensaoInvalidaException(const std::string& mensagem) : std::invalid_argument(mensagem) {}
+    };
 };
 
 #endif
