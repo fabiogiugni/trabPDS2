38c70a1406cdf14058de6c4f66984ea19a95ef5c
Ana Clara Ferreira de Sá <ferreiradesaanaclara@gmail.com>
2025-01-28 12:55:27 -0300

    Update Jogador.hpp

--- include/Jogador.hpp
+++ include/Jogador.hpp
@@ -1,44 +1,43 @@
-#ifndef JOGOTABULEIRO_H
-#define JOGOTABULEIRO_H
+#ifndef JOGADOR_H
+#define JOGADOR_H
 
-#include "Jogador.hpp"
 #include <iostream>
-#include <vector>
-#include <memory>
-#include <stdexcept> // Para exceções
+#include <string>
 
-class JogoTabuleiro {
+class Jogador
+{
 private:
-    std::shared_ptr<Jogador> jogador1, jogador2;
-    std::vector<std::vector<char>> tabuleiro;
-    int dimensaoTabuleiro;
-    bool jogadorAtual; // true para jogador1, false para jogador2
+    std::string nome;
+    std::string apelido;
+    int vitoriasReversi;
+    int vitoriasLig4;
+    int vitoriasVelha;
+    int derrotasReversi;
+    int derrotasLig4;
+    int derrotasVelha;
 
 public:
-    JogoTabuleiro(std::shared_ptr<Jogador> jogador1, std::shared_ptr<Jogador> jogador2, int n); // Construtor
-    ~JogoTabuleiro() = default;
-
-    // Leitura Jogada
-    virtual void leJogada(char linha, int coluna);
-
-    // Validação Jogada
-    virtual bool isJogadaValida(char linha, int coluna);
-
-    // Printar Tabuleiro
-    virtual void printaTabuleiro() const;
-
-    // Testa Condição de Vitória
-    virtual bool isVitoria();
-
-    // Exceções Específicas
-    class JogadaInvalidaException : public std::runtime_error {
-    public:
-        explicit JogadaInvalidaException(const std::string& mensagem) : std::runtime_error(mensagem) {}
-    };
-
-    class DimensaoInvalidaException : public std::invalid_argument {
-    public:
-        explicit DimensaoInvalidaException(const std::string& mensagem) : std::invalid_argument(mensagem) {}
-    };
+    Jogador(); //Construtor padrão
+    Jogador(const std::string& nome, const std::string& apelido); //Construtor mais completo
+    ~Jogador();
+    //Getters
+    std::string getApelido() const { return apelido; }
+    std::string getNome() const { return nome; }
+    int getVitoriasReversi() const { return vitoriasReversi; }
+    int getVitoriasLig4() const { return vitoriasLig4; }
+    int getVitoriasVelha() const { return vitoriasVelha; }
+    int getDerrotasReversi() const { return derrotasReversi; }
+    int getDerrotasLig4() const { return derrotasLig4; }
+    int getDerrotasVelha() const { return derrotasVelha; }
+    //Printar Jogador
+    void printaJogador() const;
+    //Setters (no caso somente incrementam)
+    void incrementarVitoriaReversi();
+    void incrementarVitoriaLig4();
+    void incrementarVitoriaVelha();
+    void incrementarDerrotaReversi();
+    void incrementarDerrotaLig4();
+    void incrementarDerrotaVelha();
 };
+
 #endif
