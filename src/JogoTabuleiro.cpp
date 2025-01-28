b05f7d0c8a63c02918ef15921de2c5500861bfa6
Ana Clara Ferreira de Sá <ferreiradesaanaclara@gmail.com>
2025-01-28 12:38:33 -0300

    Melhoria do tratamento de exceções JogoTabuleiro.cpp

--- src/JogoTabuleiro.cpp
+++ src/JogoTabuleiro.cpp
@@ -1,28 +1,35 @@
 #include "JogoTabuleiro.hpp"
 
 JogoTabuleiro::JogoTabuleiro(std::shared_ptr<Jogador> jogador1, std::shared_ptr<Jogador> jogador2, int n)
-    : jogador1(jogador1), jogador2(jogador2), tabuleiro(n, std::vector<char>(n, '-')), dimensaoTabuleiro(n), jogadorAtual(true) {}
+    : jogador1(jogador1), jogador2(jogador2), dimensaoTabuleiro(n), jogadorAtual(true) {
+    if (n <= 0) {
+        throw DimensaoInvalidaException("A dimensão do tabuleiro deve ser maior que zero.");
+    }
 
+    tabuleiro = std::vector<std::vector<char>>(n, std::vector<char>(n, '-'));
+}
 
 bool JogoTabuleiro::isJogadaValida(char linha, int coluna) {
-    if (linha >= 'A' && linha < 'A' + dimensaoTabuleiro &&
-        coluna >= 1 && coluna <= dimensaoTabuleiro &&
-        tabuleiro[linha - 'A'][coluna - 1] == '-') {
-        return true;
+    if (linha < 'A' || linha >= 'A' + dimensaoTabuleiro || coluna < 1 || coluna > dimensaoTabuleiro) {
+        throw JogadaInvalidaException("Coordenadas fora dos limites do tabuleiro.");
     }
-    return false;
+
+    if (tabuleiro[linha - 'A'][coluna - 1] != '-') {
+        throw JogadaInvalidaException("A posição escolhida já está ocupada.");
+    }
+
+    return true;
 }
 
 void JogoTabuleiro::leJogada(char linha, int coluna) {
-    if (isJogadaValida(linha, coluna)) {
-        // Definir o símbolo do jogador atual
-        char simbolo = jogadorAtual ? 'X' : 'O';
-        tabuleiro[linha - 'A'][coluna - 1] = simbolo;
-
-        // Alternar o jogador
-        jogadorAtual = !jogadorAtual;
-    } else {
-        std::cout << "ERRO: Jogada Inválida" << std::endl;
+    try {
+        if (isJogadaValida(linha, coluna)) {
+            char simbolo = jogadorAtual ? 'X' : 'O';
+            tabuleiro[linha - 'A'][coluna - 1] = simbolo;
+            jogadorAtual = !jogadorAtual;
+        }
+    } catch (const JogadaInvalidaException& e) {
+        std::cerr << "Erro: " << e.what() << std::endl;
     }
 }
 
@@ -33,4 +40,9 @@ void JogoTabuleiro::printaTabuleiro() const {
         }
         std::cout << "\n";
     }
+}
+
+bool JogoTabuleiro::isVitoria() {
+    // Este método seria implementado de acordo com as regras do jogo específico.
+    return false;
 }
